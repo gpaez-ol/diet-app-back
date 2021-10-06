fd9fac7a1b0b9c334b86a9bf1caeb329a12f6dd3
Gustavo <tavo@outlandhq.como>
2021-10-06 12:14:03 -0500

    ✨  Added 5000 port

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace AlgoFit
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://[*]:5000");
                 });
     }
 }
