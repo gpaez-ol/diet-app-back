34db418ae766ca87d74063a7ceaea1a2809ee0c8
Gustavo <tavo@outlandhq.como>
2021-10-06 12:44:20 -0500

    ✨  Added Health Controller

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AlgoFit.Data.DTO;
+using AlgoFit.Data.Models;
+using AlgoFit.Utils.Enums;
+namespace AlgoFit.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        public HealthController()
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(object), 400)]
+        [ProducesResponseType(401)]
+        public ActionResult HealthCheck(LoginDTO login)
+        {
+            return Ok();
+        }
+    }
+}
